9c101c578bf968658246653374bcc1b82c2821dd
kaynaremre <emrekaynar_8@hotmail.com>
2022-01-28 18:22:17 +0300

    needy item

--- multi_path.py
+++ multi_path.py
@@ -10,6 +10,12 @@ async def read_user_item(
     if q:
         item.update({"q": q})
     if not short:
-        item.update({"bol bol", "bool"})
+        item.update({"bol bol": "bool"})
     return item
 
+# http://127.0.0.1:8000/users/31/items/asd?q=hey&short=True
+
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy} # Needy is neccessary !!!!!
+    return item
