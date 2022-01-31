f0f8501b63a16382ea02760c78839dd7d4221d6c
kaynaremre <emrekaynar_8@hotmail.com>
2022-01-31 16:40:20 +0300

    put and put with query

--- request_body.py
+++ request_body.py
@@ -12,6 +12,8 @@ class Item(BaseModel):
 
 app = FastAPI()
 
+# POST is for creating a new item
+
 
 @app.post("/items/")
 async def create_item(item: Item):
@@ -23,3 +25,22 @@ async def create_item(item: Item):
     return item_dict
 
 # In order to test POST, go to localhost:8000/docs and Try it out, Execute :)
+
+# PUT can be used for update operations
+
+
+@app.put("/items/{item_id}")
+async def create_itemm(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+
+# PUT with query
+@app.put("/item/{item_id}")
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        result.update({"price_with_tax": price_with_tax})
+    if q:
+        result.update({"q": q})
+    return result
